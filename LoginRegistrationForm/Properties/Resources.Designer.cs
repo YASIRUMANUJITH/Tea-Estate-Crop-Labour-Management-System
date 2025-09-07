3b58fcbc4dc31b64f92ffd1a87f13de03e670663
Senali-Jay <jasvjayasundara@students.nsbm.ac.lk>
2025-09-07 13:45:33 +0530

    update logo and design

--- LoginRegistrationForm/Properties/Resources.Designer.cs
+++ LoginRegistrationForm/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace LoginRegistrationForm.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap logo3 {
+            get {
+                object obj = ResourceManager.GetObject("logo3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
